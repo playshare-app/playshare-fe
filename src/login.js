cd3c3f4f6759275dd57eec5e0eb66604fa007293
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-03-08 19:46:13 -0300

    added routes, homepage, oAuth

---
+++ src/login.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { logInUser } from './api-utils';
+
+export default class login extends Component {
+  state = {
+    email: '',
+    password: ''
+  };
+
+  handleEmailChange = (e) => this.setState({ email: e.target.value });
+
+  handlePasswordChange = (e) => this.setState({ password: e.target.value });
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await logInUser(this.state.email, this.state.password);
+      console.log(user);
+
+      //   this.props.history.push('/');
+    } catch (e) {
+      this.setState({ error: e.response.body });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Email</label>
+          <input
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+            type="email"
+          ></input>
+          <label>Password</label>
+          <input
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+            type="password"
+          ></input>
+          <button>Spotify Login</button>
+        </form>
+      </div>
+    );
+  }
+}
